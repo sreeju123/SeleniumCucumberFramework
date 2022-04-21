5b427c250a018c1069cf8963c92db010b5580be1
sreeju123 <sreejithsreerolla@gmail.com>
2022-04-21 19:56:40 +0530

    Modified runner file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.testng.annotations.DataProvider;
  * @author Sreej
  */
 
-@CucumberOptions(features = "src/test/resources/features", glue = "steps",
+@CucumberOptions(features = "src/test/resources/features", glue = "steps", monochrome = true, strict = true,
         plugin = {"json:src/test/resources/cucumberReport/cucumberReport.json", "listeners.BDDTestListener"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
